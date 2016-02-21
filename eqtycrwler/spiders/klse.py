870ccf8ffe42651748be5f385fbdaa1a6d82c63d
msh <mohd.shukri@outlook.com>
2016-02-21 19:05:32 +0800

    Added to function to obtain profile from another site in case yahoo does not have it.

--- eqtycrwler/spiders/klse.py
+++ eqtycrwler/spiders/klse.py
@@ -8,14 +8,18 @@ from eqtycrwler.items import EqtycrwlerItem
 class KlseSpider(scrapy.Spider):
     name = 'klse'
     allowed_domains = ['bursamalaysia.com',
-                       'finance.yahoo.com']
+                       'finance.yahoo.com',
+                       'msn.com']
     curr_page = 1
+    tbl_url = 'http://ws.bursamalaysia.com/market/securities/equities/prices/prices_f.html?page=%d'
     yhoo_suffix = 'KL'
     yhoo_url = 'http://finance.yahoo.com/q/pr?s=%s+Profile'
-    tbl_url = 'http://ws.bursamalaysia.com/market/securities/equities/prices/prices_f.html?page=%d'
+    # details of fallback url if yahoo does not have a profile
+    fb_suffix = 'KLS'
+    fb_url = 'http://www.msn.com/en-us/money/stockdetails/company/fi-135.1.%s'
 
     def start_requests(self):
-        return [scrapy.Request(self.tbl_url + '1', callback=self.parse_table_rows)]
+        return [scrapy.Request(self.tbl_url % 1, callback=self.parse_table_rows)]
 
     def parse_table_rows(self, response):
         # Examine the table rows returned from the jquery call
@@ -36,5 +40,18 @@ class KlseSpider(scrapy.Spider):
 
     def parse_profile(self, response):
         item = response.meta['item']
-        item['yhoo_profile'] = response.xpath('//*[(@id = "yfncsumtab")]//p/text()').extract_first()
+        profile = response.xpath('//*[(@id = "yfncsumtab")]//p/text()').extract_first()
+        if not ('There is no Profile data available' in profile):
+            item['yhoo_profile'] = profile
+            yield item
+        else:
+            self.logger.info("No profile found for %s on Yahoo" % item['short_name'])
+            yield scrapy.Request(self.fb_url % '.'.join([item['code'], self.fb_suffix]),
+                                 callback=self.fallback_profile,
+                                 meta={'item': item})
+
+    def fallback_profile(self, response):
+        item = response.meta['item']
+        item['yhoo_profile'] = response.xpath(
+            '//*[contains(concat( " ", @class, " " ), concat( " ", "toggle-text", " " ))]/text()').extract_first()
         yield item
